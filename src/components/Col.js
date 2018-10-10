c58ee8c3eb7169743a01b37fd13a0ca71c6af9f9
Jeremy Avenarius <jeremyavenarius@gmail.com>
2018-10-09 19:01:40 -0600

    Update Col.js
    spelling error

--- src/components/Col.js
+++ src/components/Col.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// This Col component offers us the convinience of being able to set a column's "size" prop instead of its className
+// This Col component offers us the convenience of being able to set a column's "size" prop instead of its className
 // We can also omit the col- at the start of each Bootstrap column class, e.g. size="md-12" instead of className="col-md-12"
 
 const Col = props => {
