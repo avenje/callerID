c77814c2139350880f80955a32b66af9b44c861f
avenje <jeremyavenarius@gmail.com>
2020-04-27 21:24:53 -0400

    update api2

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import {APIKEY} from "../secrets.js";
 
-const BASEURL = "https://api.ekata.com/3.1/phone.json?";
+const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
 // const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
+// const BASEURL = "https://api.ekata.com/3.1/phone.json?";
+
 
 export default {
   search: function(query) {
