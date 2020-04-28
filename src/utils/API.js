4df24105bd96efd727c6d4aee470140e481b4ab8
Jeremy Avenarius <jeremyavenarius@gmail.com>
2020-04-27 22:00:36 -0400

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {APIKEY} from "../secrets.js";
+// import {APIKEY} from "../secrets.js";
 
 // const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
 // const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
