95e760fe7fa96854966bb1893de85eebcc79507c
Jeremy Avenarius <jeremyavenarius@gmail.com>
2020-04-27 21:57:51 -0400

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import {APIKEY} from "../secrets.js";
 
-const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
+// const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
 // const BASEURL = "https://proapi.whitepages.com/3.0/phone.json?";
 // const BASEURL = "https://api.ekata.com/3.1/phone.json?";
 
 
 export default {
   search: function(query) {
-    return axios.get(BASEURL + APIKEY + "&phone=" + query);
+    return axios.get(BASEURL + "api_key=53df466206a24645ba9c34142e292560&phone=" + query);
   }
 };
